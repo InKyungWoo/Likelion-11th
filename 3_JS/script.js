ac98fd46c7983d81f9a23ce09ec8ec282df5aa4d
Inkyung <chelsea1201@naver.com>
2023-05-12 10:39:52 +0900

    JS세션 과제

--- 3_JS/script.js
+++ 3_JS/script.js
@@ -6,7 +6,11 @@ function getTime() {
   let minute = padZero(now.getMinutes());
   let second = padZero(now.getSeconds());
 
-  timeDiv.textContent = `${hour}시 ${minute}분 ${second}초`;
+  var dd = now.getDate();
+	var mm = now.getMonth()+1; 
+	var yyyy = now.getFullYear();
+
+  timeDiv.textContent = `${yyyy}년 ${mm}월 ${dd}일 \n ${hour}시 ${minute}분 ${second}초`;
 
 	setInterval(getTime, 1000);
 }
